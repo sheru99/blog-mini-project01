430267d17ed23d4f7a46eed28ccf108afb7df20d
mdyousuf9007 <106535726+mdyousuf9007@users.noreply.github.com>
2022-09-12 00:10:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://kusum_99:9vJ9mxlJH1cYZ1oO@cluster0.jelghm1.mongodb.net/project1-blog", {
+mongoose.connect("mongodb+srv://mdyousuf9007:yousuf9007@cluster0.0ldwd.mongodb.net/project-1-blog", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
